88de54b43a99cad24f3784d2e86c647edb441693
Richi <tremon36xd@gmail.com>
2024-02-17 15:26:28 +0100

    updated

--- useful2.cpp
+++ useful2.cpp
@@ -5,8 +5,7 @@
 #include <iostream>
 
 class DDS {
-
-    public:
+    public: 
 
     enum notas {
         DO = 0,
@@ -18,7 +17,17 @@ class DDS {
         SI = 6
     };
 
-    DDS(int n_points, int* steps, int step_amount) {
+    virtual void update() = 0;
+    //virtual void setNote(notas nota) = 0;
+};
+
+
+class LutDDS : public DDS {
+
+    public:
+
+    
+    LutDDS(int n_points, int* steps, int step_amount) {
         steps_lut = new int[step_amount];
         points = n_points;
         memcpy(steps_lut,steps,step_amount * sizeof(int));
@@ -55,14 +64,55 @@ class DDS {
 
 };
 
+class RealtimeDDS : public DDS {
+
+    public:
+
+    RealtimeDDS(float (*ft)(float t),float period, float step) {
+        this->period = period;
+        this->step = step;
+        this->f = ft;
+    }
+
+    void update() {
+
+        // PWM write position @TODO
+        std::cout << "t = " << current_time;
+        std::cout << "; generated -> " << f(current_time) << std::endl;
+
+        current_time += step; // @TODO add multiplier for each note
+        if(current_time>=period) {
+            current_time-= period;
+        }
+
+
+        
+    }
+
+    private:
+
+    float (*f)(float t); // function of the dds
+    float period;
+    float step;
+    float current_time = 0;
+
+
+};
+
 int M[7] = {1,2,3,4,5,6,7};
+
+float sine(float t) {
+    const float original_f = 1; // 1Hz original period
+    return sin(2*M_PI*original_f*t);
+}
 
 int main(void) {
     int a;
-    DDS dds(100,M,7);
-    dds.setNote(dds.MI);
+    //DDS* dds = new LutDDS(100,M,7);
+    DDS* dds = new RealtimeDDS(sine,2.546479,0.2);
+    //dds->setNote(DDS::MI);
     for(int i = 0; i < 120; i++) {
-        dds.update();
+        dds->update();
     }
 
     return 0;
