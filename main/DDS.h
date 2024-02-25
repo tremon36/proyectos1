11f2539d3e2f3d1292a88d4ffd50fad6c3bc5696
Richi <tremon36xd@gmail.com>
2024-02-25 10:12:17 +0100

    cleanup

--- main/DDS.h
+++ main/DDS.h
@@ -1,13 +1,14 @@
+#ifndef _DDS_H
+#define _DDS_H
+
 #include <stdio.h>
 #include <math.h>
-#include <iostream>
-#include <memory>
 
 class DDS {
 
     public: 
 
-    const int pwm_max = 1000;
+    const int pwm_max = 511;
 
     enum notas {
         DO = 0,
@@ -18,9 +19,10 @@ class DDS {
         FA = 5, 
         FASHARP = 6, 
         SOL = 7,
-        LA = 8,
-        LAHARP = 9,
-        SI = 10
+        SOLSHARP = 8,
+        LA = 9,
+        LAHARP = 10,
+        SI = 11
     };
 
     // Some example functions that can be used for the DDS
@@ -52,13 +54,13 @@ class DDS {
     virtual int update() = 0;
     virtual void setNote(notas nota) = 0;
     virtual void enableRealtimeVolumeControl(bool enable) = 0;
-    virtual void setVolumePercent(float percent) = 0;
+    virtual void setVolumePercent(int percent) = 0;
 
 
     protected:
 
         bool volume_control_enabled = 0;
-        float volume;
+        int volume;
         int current_note;
 };
 
@@ -75,8 +77,8 @@ class LutDDS : public DDS {
         // Generate LUT according to period, function and number of points
 
         this->LUT = new uint16_t[npoints]; // using smart pointers here would just make the code either slower (std::vector) or unreadable
-        float min_fn = std::numeric_limits<float>::max();
-        float max_fn = std::numeric_limits<float>::min();
+        float min_fn = 9000000.0f;
+        float max_fn = -9000000.0f;
 
         for(int i = 0; i < npoints; i++) { // get maximum and minimum of function for normalization
             float aux = 0.0f;
@@ -86,13 +88,13 @@ class LutDDS : public DDS {
         }
 
         for(int i = 0; i < npoints; i++) {
-            this->LUT[i] = (int) ((ft(period * i/npoints) - min_fn) * (floorf32(pwm_max * original_volume)) / (max_fn - min_fn)); //function normalization and lut fill
+            this->LUT[i] = (int) ((ft(period * i/npoints) - min_fn) * (floor(pwm_max * original_volume)) / (max_fn - min_fn)); //function normalization and lut fill
         }
 
         // generate steps LUT
 
-        for(int i = 0; i < 11; i++) {
-            this->steps_lut[i] = (int) (npoints * step  / period) * powf32(1.06f,i);
+        for(int i = 0; i < sizeof(steps_lut) / sizeof(int); i++) {
+            this->steps_lut[i] = (int) ((npoints * step  / period) * pow(1.06f,i));
         }
 
     }
@@ -103,22 +105,20 @@ class LutDDS : public DDS {
         delete LUT;
     }
 
+
     int update() {
-        // PWM write position @TODO
         int result = 0;
         if(!volume_control_enabled) {
-            //std::cout << "pos = " << position << "->" << this->LUT[position] << std::endl;
-            result =  this->LUT[position];
+            result = this->LUT[position];
         } else {
-            //std::cout << "pos = " << position << "->" << std::min((int)(this->LUT[position] * volume),pwm_max) << std::endl;
-            result = std::min((int)(this->LUT[position] * volume),pwm_max);
+            result = min((int)(LUT[position] * volume / 100),pwm_max);
         }
-
 
         position = position + steps_lut[current_note];
         if(position >= points) position -= points;
 
         return result;
+             
     }
 
     void setNote(notas nota) {
@@ -129,7 +129,7 @@ class LutDDS : public DDS {
         this->volume_control_enabled = true;
     }
 
-    void setVolumePercent(float percent) {
+    void setVolumePercent(int percent) {
         this->volume = percent;
     }
 
@@ -137,7 +137,7 @@ class LutDDS : public DDS {
     private:
 
     int points;
-    int steps_lut[11];
+    int steps_lut[12];
     int amount;
     int position;
     uint16_t* LUT;
@@ -156,28 +156,16 @@ class RealtimeDDS : public DDS {
     }
 
     int update() {
-
-        // PWM write position @TODO
-        //std::cout << "t = " << current_time;
-        int result = 0;
-        if(!volume_control_enabled) {
-            //std::cout << "; generated -> " << (int) ((f(current_time) - ft_min) * pwm_max / (ft_max-ft_min)) << std::endl;
-            result =  (int) ((f(current_time) - ft_min) * pwm_max / (ft_max-ft_min));
-        } else {
-            //std::cout << "; generated -> " << std::min(((int) ((f(current_time) - ft_min) * pwm_max / (ft_max-ft_min) * volume)),pwm_max)<< std::endl;
-            result =  std::min(((int) ((f(current_time) - ft_min) * pwm_max / (ft_max-ft_min) * volume)),pwm_max);
-        }
+        return 0; //@TODO
 
         current_time += step * multiplier;
         if(current_time>=period) {
             current_time-= period;
-        }   
-
-        return result; 
+        }    
     }
 
     void setNote(notas nota) {
-        this->multiplier = powf32(1.06f,nota);
+        this->multiplier = pow(1.06f,nota);
     }
 
     void enableRealtimeVolumeControl(bool enable) {
@@ -196,7 +184,7 @@ class RealtimeDDS : public DDS {
     float current_time = 0;
     float multiplier = 0;
     bool volume_control_enabled = 0;
-    float volume;
+    int volume;
     float ft_max = 1;
     float ft_min = 0;
 
@@ -214,7 +202,7 @@ class Mixer {
             synths[channel_number] = synthetizer;
         }
 
-        void setChannelVolume(int channel_number, float volume) {
+        void setChannelVolume(int channel_number, int volume) {
             
             channel_volumes[channel_number] = volume;
             synths[channel_number]->enableRealtimeVolumeControl(true);
@@ -226,17 +214,18 @@ class Mixer {
             int acc = 0;
             for(int i = 0; i < n_channels; i++) {
                 val = synths[i]->update();
-                std::cout << "synth " << i << " -> " << val << std::endl;
                 acc += val;
             }
-            std::cout << "mixer ->" << acc << std::endl;
+            //ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, acc);
+            //ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            // @TODO PWM WRITE
+
         }
 
 
     private:
         DDS* synths[n_channels];
-        float channel_volumes[n_channels];
+        int channel_volumes[n_channels];
 };
 
-
-
+#endif
--- useful2.cpp
+++ useful2.cpp
@@ -4,20 +4,31 @@
 #include <memory>
 #include "DDS.h"
 
+ float sine(float t) { //  Se asume que la frecuencia de la funcion es do grave.
+        const float original_f = 1.0f; // F do grave
+        return sin(2*M_PI*original_f*t);
+    }
+
 
 int main(void) {
     int a;
     //std::unique_ptr<DDS> dds = std::make_unique<LutDDS>(sine,0.5f,0.001f,10000,1.0f);
     //std::unique_ptr<DDS> dds = std::make_unique<RealtimeDDS>(DDS::triangle,1.0f/50000.0f,0.0f,1.0f); // 50 Khz ticker
-    std::unique_ptr<DDS> dds = std::make_unique<LutDDS>(DDS::sine,1.0f/50000.0f,10000);
-    
-    dds->setNote(DDS::SI);
-    dds->enableRealtimeVolumeControl(true);
-    dds->setVolumePercent(0.5f);
+    DDS* dds =new LutDDS(sine,1.0f/100.0f,500,1.0f,1.0f);//(DDS::sine,1.0f/50000.0f,10000);
+    DDS* dds2 =new LutDDS(sine,1.0f/100.0f,500,1.0f,1.0f);
+    dds->setNote(DDS::DO);
+    dds2->setNote(DDS::SI);
 
-    for(float time = 0; time < 1.0f/261.63f; time += 1.0f/50000.0f) {
-        dds->update();
-        //std::cout << "t = " << time << " -> " << DDS::triangle(time) << std::endl;
+    Mixer<2> mixer;
+    mixer.setChannelSynth(0,dds);
+    mixer.setChannelSynth(1,dds2);
+
+    mixer.setChannelVolume(0,0.25f);
+    mixer.setChannelVolume(1,0.5f);
+
+    for(float time = 0; time < 1.0f; time += 1.0f/100.0f) {
+        mixer.update();
+        //std::cout << "t = " << time << " -> " <<  << std::endl;
     }
     return 0;
 }
